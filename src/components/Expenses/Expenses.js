5c960f18a13f9a3a5356768b5791b1c61c82a4d3
Leila Samih <LeilaS@algoconsulting.net>
2022-08-04 09:30:30 +0100

    Update list key

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,6 +16,7 @@ const Expenses = (props) => {
       <ExpensesFilter selected={year} onSelectYear={selectYearHandler} />
       {props.items.map((expense) => (
         <ExpenseItem
+          key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
