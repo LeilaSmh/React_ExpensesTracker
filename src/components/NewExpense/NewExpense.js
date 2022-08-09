29f1b67572b104ea8fb335ed9a2551661b16b59c
Leila Samih <LeilaS@algoconsulting.net>
2022-08-09 09:41:45 +0100

    Cancel Form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,36 @@
 import "./NewExpense.css";
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [showForm, setShowForm] = useState(false);
+
   const saveNewExpenseHandler = (data) => {
     const expenseData = {
       ...data,
       id: Math.random().toString(),
     };
     props.onAddNewExpense(expenseData);
+    setShowForm(false);
+  };
+
+  const showFormHandler = () => {
+    setShowForm(true);
+  };
+
+  const cancelFormHandler = () => {
+    setShowForm(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveNewExpense={saveNewExpenseHandler} />
+      {!showForm && <button onClick={showFormHandler}>Add New Expense</button>}
+      {showForm && (
+        <ExpenseForm
+          onSaveNewExpense={saveNewExpenseHandler}
+          onCancel={cancelFormHandler}
+        />
+      )}
     </div>
   );
 };
